2e53d65e23c6024c05f62577a1c1d0ee1d2d2897
Anuraag001 <anuraagbv1@gmail.com>
2023-09-07 20:43:59 +0530

    Symbol Table\'s line_number made to Linked List

--- sym_table.c
+++ sym_table.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+typedef struct Line{
+    int line;
+    struct Line* next;
+} Line;
 
 typedef struct symbol {
     char name[100];
     char datatype[100];
     int value;
-    int line_number[5];
+    Line* line_num;
     int scope;
     struct symbol* next;
 } symbol;
@@ -18,23 +24,41 @@ void initialize() {
         hash[i] = NULL;
     }
 }
+Line* create_ll(int c){
+    Line* temp=(Line*) malloc(sizeof(Line));
+    temp->line=c;
+    temp->next=NULL;
+    return temp;
+}
 
-symbol* create(const char* str,int length,char* type,int line_num) {
+symbol* create(const char* str,int length,char* type) {
     symbol* temp = (symbol*)malloc(sizeof(symbol));
     strcpy(temp->name, str);
     strcpy(temp->datatype,type);
     temp->value = length;
-    temp->line_number[0]=line_num;
+    temp->line_num=NULL;
+    //temp->line_number[0]=line_num;
     temp->next = NULL;
     temp->scope=-1;
-    for (int i = 1; i < 5; i++) {
+    /*for (int i = 1; i < 5; i++) {
         temp->line_number[i] = 0;
-    }
+    }*/
+
     return temp;
 }
+symbol* present(char str[],int check){
 
-void hash_algo(char str[],int length,char type[],int line_num) {
-    symbol* temp = create(str,length,type,line_num);
+    symbol* ptr=hash[check];
+    while(ptr!=NULL){
+        if(!strcmp(ptr->name,str)) return ptr;
+        ptr=ptr->next;
+    }
+
+    return NULL;
+
+}
+void hash_algo(char str[],int length,char type[],int line_p) {
+    symbol* temp = create(str,length,type);
     int i = 0, c, ans = 0;
     while (str[i] != '\0') {
         c = str[i];
@@ -44,12 +68,25 @@ void hash_algo(char str[],int length,char type[],int line_num) {
     ans %= 100;
     if (hash[ans] == NULL) {
         hash[ans] = temp;
-    } else {
+        temp->line_num=create_ll(line_p);
+        return;
+    } 
+    if(present(str,ans)){
+        symbol* p=present(str,ans);
+        Line* ptr=p->line_num;
+        if(ptr==NULL) ptr=create_ll(line_p);
+        while(ptr->next!=NULL) ptr=ptr->next;
+
+        ptr->next=create_ll(line_p);
+        
+    }
+    else {
         symbol* ptr = hash[ans];
         while (ptr->next != NULL) {
             ptr = ptr->next;
         }
         ptr->next = temp;
+        temp->line_num=create_ll(line_p);
     }
 }
 
