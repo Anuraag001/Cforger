2bcbf0341de6ce34277981e784317c9b8b44e433
Anuraag001 <anuraagbv1@gmail.com>
2023-09-10 14:46:56 +0530

    Changes some naming conventions in symbol table

--- sym_table.c
+++ sym_table.c
@@ -10,8 +10,8 @@ typedef struct Line{
 
 typedef struct symbol {
     char name[100];
-    char datatype[100];
-    int value;
+    char token[100];
+    int length;
     Line* line_num;
     int scope;
     struct symbol* next;
@@ -31,18 +31,14 @@ Line* create_ll(int c){
     return temp;
 }
 
-symbol* create(const char* str,int length,char* type) {
+symbol* create(const char* str,int len,char* type) {
     symbol* temp = (symbol*)malloc(sizeof(symbol));
     strcpy(temp->name, str);
-    strcpy(temp->datatype,type);
-    temp->value = length;
+    strcpy(temp->token,type);
+    temp->length = len;
     temp->line_num=NULL;
-    //temp->line_number[0]=line_num;
     temp->next = NULL;
     temp->scope=-1;
-    /*for (int i = 1; i < 5; i++) {
-        temp->line_number[i] = 0;
-    }*/
 
     return temp;
 }
@@ -90,62 +86,4 @@ void hash_algo(char str[],int length,char type[],int line_p) {
     }
 }
 
-/*int main() {
-    initialize();
-
-    char string[][100] = {
-        "apple",
-        "banana",
-        "cherry",
-        "date",
-        "elderberry",
-        "fig",
-        "grape",
-        "honeydew",
-        "kiwi",
-        "lemon",
-        "mango",
-        "nectarine",
-        "orange",
-        "pear",
-        "quince",
-        "raspberry",
-        "strawberry",
-        "tangerine",
-        "ugli",
-        "watermelon",
-        "xylophone",
-        "yellow",
-        "zebra",
-        "apple pie",
-        "blueberry",
-        "cantaloupe",
-        "dragonfruit",
-        "elephant",
-        "fruit salad"
-    };
-    for (int i = 0; i < 29; i++) {
-        hash_algo(string[i]);
-    }
-
-    for (int i = 0; i < 20; i++) {
-        printf("Index %d", i);
-        symbol* current = hash[i];
-        while (current != NULL) {
-            printf("->%s", current->name);
-            current = current->next;
-        }
-        printf("\n");
-    }
-
-    // for (int i = 0; i < 100; i++) {
-    //     symbol* current = hash[i];
-    //     while (current != NULL) {
-    //         symbol* temp = current;
-    //         current = current->next;
-    //     }
-    // }
-
-    return 0;
-}*/
 
